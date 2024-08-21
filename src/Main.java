6cd7ecb6fb64b0e9f4340aa7f887ee46f62d9ae3
Gabriel Henrique Mombelli <gabriel.mombelli9@gmail.com>
2024-08-21 20:53:44 -0300

    Códigos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Taffezinho";
+
+        /*
+        * >     Maior
+        * >=    Maior igual
+        * <     Menor
+        * <=    Menor igual
+        * ==    É igual
+        * !=    É diferente
+        * equal Compara textos
+        *
+        */
+
+        if(idadePessoa >= 21){
+            System.out.println("É maior que 21");
+        }else{
+            System.out.println("Não é maior que 20");
+        }
+
+
+
     }
 }
